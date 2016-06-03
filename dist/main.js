49516c5c650270030143fc0c883e9435e74f987b
Muhammad Usman <muhammad.usman@widespace.com>
2016-06-03 14:29:45 +0200

    let commit dist folder as well

---
+++ dist/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var script1 = function script1() {
+  console.log('script1.js');
+};
+'use strict';
+
+var script1 = function script1() {
+  console.log('script2.js');
+};
+//# sourceMappingURL=main.js.map
